3f9110fbee368d6cf4629451de98a288e58da09c
Carlos Gochez <carlos.gochez@unitec.edu>
2012-10-16 07:33:13 -0600

    Agregar la clase Lex Luthor

---
+++ src/LexLuthor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gotcha
+ */
+public class LexLuthor {
+    public int killSuperman(){
+        System.out.println("Moritee");
+        return 1;
+    }
+}
