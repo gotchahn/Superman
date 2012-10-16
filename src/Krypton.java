0e79487c7e084c1902204aff59bb61690ee3ee3b
Carlos Gochez <carlos.gochez@unitec.edu>
2012-10-16 07:28:47 -0600

    Creacion Inicial de Superman Project

---
+++ src/Krypton.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Krypton {
+    public double potencia;
+    
+    public void debilitar(){
+        System.out.println("Superman se pone mal");
+    }
+}
